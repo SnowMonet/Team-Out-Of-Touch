5ed53012715d6dfa42199ce71a02d40a76017987
Dazariath <c17366881@mydit.ie>
2019-10-12 22:40:27 +0100

    added Character Placeholder model and added movement script

---
+++ Out of Touch/Assets/CharacterMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float CharacterMF;               //CF = movement force
+    public Rigidbody CharacterRB;                 //RB=Rigidbody
+
+    void Start()
+    {
+        CharacterRB = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("w"))
+        {
+            CharacterRB.AddForce(Vector3.forward * CharacterMF);
+        }
+
+        if (Input.GetKeyDown("s"))
+        {
+            CharacterRB.AddForce(Vector3.back * CharacterMF);
+        }
+
+        if (Input.GetKeyDown("a"))
+        {
+            CharacterRB.AddForce(Vector3.left * CharacterMF);
+        }
+
+        if (Input.GetKeyDown("d"))
+        {
+            CharacterRB.AddForce(Vector3.right * CharacterMF);
+        }
+    }
+}
