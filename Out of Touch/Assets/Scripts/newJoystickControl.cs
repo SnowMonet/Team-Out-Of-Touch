185e5ae88767a1095d38ec2e6f4eef0c07f4c679
jonanobonano <c16743131>
2019-10-16 17:46:06 +0100

    virtual joystick movement and camera swipe
    movement controls for mobile. I duplicated character and put code on bone element, works for me, not brilliant but a start

---
+++ Out of Touch/Assets/Scripts/newJoystickControl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class newJoystickControl : MonoBehaviour
+{
+    public Joystick joystick;
+    //public Joybutton joybutton;
+   
+    public float veloRate = 4.0f;
+
+    public FixedTouchField TouchField;
+    public float CameraAngle;
+    //public float xCameraAngle;
+   // public float yCameraAngle;
+    public float CameraAngleSpeed = 0.2f;
+
+
+    public Rigidbody myRb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        joystick = FindObjectOfType<Joystick>();
+        //joybutton = FindObjectOfType<Joybutton>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myRb = GetComponent<Rigidbody>();
+        myRb.velocity = new Vector3(joystick.Horizontal * veloRate, myRb.velocity.y, joystick.Vertical * veloRate);
+        
+        CameraAngle += TouchField.TouchDist.x * CameraAngleSpeed;
+        //CameraAngle += TouchField.TouchDist.y * CameraAngleSpeed;
+        // xCameraAngle += TouchField.TouchDist.x * CameraAngleSpeed;
+        //yCameraAngle += TouchField.TouchDist.y * CameraAngleSpeed;
+
+        Camera.main.transform.position = transform.position + Quaternion.AngleAxis(CameraAngle, Vector3.up) * new Vector3(0, 12, -18);
+        Camera.main.transform.rotation = Quaternion.LookRotation(transform.position + Vector3.up * 2f - Camera.main.transform.position, Vector3.up);
+    }
+}
